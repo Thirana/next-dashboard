54f1fc437f5c55b2bf26dd61d6b6ad628f8dbaf4
Thirana <t.embuldeniya@gmail.com>
2023-12-08 06:22:04 +0530

    dashboard UI Setup

--- app/dashboard/page.jsx
+++ app/dashboard/page.jsx
@@ -1,7 +1,26 @@
-import React from "react";
+import Card from "../ui/dashboard/card/card";
+import Chart from "../ui/dashboard/chart/chart";
+import styles from "../ui/dashboard/dashboard.module.css";
+import RightBar from "../ui/dashboard/rightbar/rightbar";
+import Transaction from "../ui/dashboard/transaction/transaction";
 
 const Dashboard = () => {
-  return <div>Dashboard</div>;
+  return (
+    <div className={styles.wrapper}>
+      <div className={styles.main}>
+        <div className={styles.card}>
+          <Card />
+          <Card />
+          <Card />
+        </div>
+        <Transaction />
+        <Chart />
+      </div>
+      <div className={styles.side}>
+        <RightBar />
+      </div>
+    </div>
+  );
 };
 
 export default Dashboard;
--- app/ui/dashboard/sidebar/sidebar.jsx
+++ app/ui/dashboard/sidebar/sidebar.jsx
@@ -104,6 +104,11 @@ const Sidebar = () => {
           </li>
         ))}
       </ul>
+
+      <button className={styles.logout}>
+        <MdLogout />
+        Logout
+      </button>
     </div>
   );
 };
